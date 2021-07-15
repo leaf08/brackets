a5684aa9b8ceb38732e6ab77ea2aa4cf7c1eb1c6
Leaf08 <leaf08@yandex.ru>
2021-07-15 12:05:39 +0300

    init: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 > 0) return false;
+  stack = [];
+  for (let sym of str) {
+    bracketsConfig.map(bracket => {
+      if(sym === bracket[0]) {
+        if(bracket[0] !== bracket[1]) stack.push(sym);
+        else if(bracket[0] !== stack[stack.length-1]) stack.push(sym);
+        else  stack.pop();
+      }
+      else if(sym === bracket[1] && bracket[0] === stack[stack.length-1]) {
+        stack.pop();
+      }
+    });
+  }
+  if(stack.length === 0) return true;
+  return false;
 }
